227336ed6d2359058546cccfaf02d7b335ff075d
RayanBanat <rayanbanat57@gmail.com>
2025-09-14 05:00:47 +0300

    nav

---
+++ store/src/componants/nav.js
@@ -0,0 +1,21 @@
+// class componant
+// v14
+// function componant
+// function name(arg){
+// return(
+// html elemnt<></>
+// )
+// }
+// export
+import '../css/nav.css'
+export default function Nav (){
+    return (
+        <div className="nav">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    );
+}
